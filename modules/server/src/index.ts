b3b2c018a77d3bb4188248bf99033837426eebc7
Takagi Kensuke <janus.wel.3@gmail.com>
2019-05-05 01:44:28 +0900

    :heavy_plus_sign::wrench: use Express

--- modules/server/src/index.ts
+++ modules/server/src/index.ts
@@ -1,14 +1,25 @@
-import { Todos } from '@januswel/domain'
+import * as Express from 'express'
 
-const todos = Todos.factory([
-  {
-    title: 'use RNW',
-    content: 'setup react-native-web',
-  },
-  {
-    title: 'build Todo app',
-    content: 'implement client',
-  },
-])
+const app = Express()
 
-console.log(todos)
+app.use((err: Error, _: Express.Request, res: Express.Response, __: Express.NextFunction) => {
+  console.error(err.stack)
+  res.status(500).send('Internal Server Error')
+})
+
+app.get('/', (_: Express.Request, res: Express.Response) => {
+  res.send('Hello World!')
+})
+
+app.use((err: Error, _: Express.Request, res: Express.Response, __: Express.NextFunction) => {
+  console.error(err.stack)
+  res.status(500).send('500 Internal Server Error')
+})
+
+app.use((_: Express.Request, res: Express.Response, __: Express.NextFunction) => {
+  res.status(404).send('404 Not Found')
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!')
+})
