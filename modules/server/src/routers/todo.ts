0d2c772ee42f8217574b9d7a1e64c88d6800920e
Takagi Kensuke <janus.wel.3@gmail.com>
2019-05-06 17:41:07 +0900

    :sparkles: implement getting todos

--- modules/server/src/routers/todo.ts
+++ modules/server/src/routers/todo.ts
@@ -1,21 +1,30 @@
 import * as Express from 'express'
-import { Todo } from '@januswel/domain'
+import { Todo, Todos } from '@januswel/domain'
 
 const router = Express.Router()
 
-const todos = []
+const todos = Todos.factory([])
 
 router.post('/', (req: Express.Request, res: Express.Response) => {
-  console.log(req.body)
   const { title, detail } = req.body
   const result = Todo.factory(title, detail)
   todos.push(result)
   res.status(201).json(result)
 })
 
-router.get('/', (_: Express.Request, res: Express.Response) => {
-  // TODO
-  res.sendStatus(200)
+router.get('/', (req: Express.Request, res: Express.Response) => {
+  const page = req.query.page || 1
+  const n = req.query.n || 10
+
+  const begin = (page - 1) * n
+  const end = page * n - 1
+  const result = todos.slice(begin, end)
+
+  if (result.length === 0) {
+    res.sendStatus(404)
+  }
+
+  res.status(200).json(result)
 })
 
 router.get('/:id', (_: Express.Request, res: Express.Response) => {
