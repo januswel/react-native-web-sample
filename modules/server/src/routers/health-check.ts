8ec0ee916bddbff1a18ed04e22e3e65fc695ee26
Takagi Kensuke <janus.wel.3@gmail.com>
2019-05-07 23:07:25 +0900

    :rotating_light: fix lint warnings

--- modules/server/src/routers/health-check.ts
+++ modules/server/src/routers/health-check.ts
@@ -1,9 +1,11 @@
 import * as Express from 'express'
 
+import CONSTANTS from '../constants'
+
 const router = Express.Router()
 
 router.get('/', (_: Express.Request, res: Express.Response) => {
-  res.send(200)
+  res.send(CONSTANTS.HTTP.STATUS_CODE.OK)
 })
 
 export default router
