2ee7dfceb631cf07f4179dd77b3345b2fa79e716
Takagi Kensuke <janus.wel.3@gmail.com>
2019-05-07 23:22:11 +0900

    :heavy_plus_sign::bug: fix lint errors

--- modules/server/src/middlewares/access-log.ts
+++ modules/server/src/middlewares/access-log.ts
@@ -1,8 +1,9 @@
 import * as Express from 'express'
+import logger from '../utils/logger'
 
 export default (req: Express.Request, _: Express.Response, next: Express.NextFunction) => {
   const { body } = req
   const serializedBody = Object.keys(body).length !== 0 ? JSON.stringify(body) : ''
-  console.log(`${req.method} ${req.url} ${serializedBody}`)
+  logger(`${req.method} ${req.url} ${serializedBody}`)
   next()
 }
