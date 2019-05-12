214bda888e3093ab58e129e24ffda0d39246b585
Takagi Kensuke <janus.wel.3@gmail.com>
2019-05-12 23:06:50 +0900

    :art: show time correctly

--- modules/client/src/selectors/get-todos.ts
+++ modules/client/src/selectors/get-todos.ts
@@ -11,7 +11,10 @@ const MINUTES_PER_HOUR = 60
 const HOURS_PER_DAY = 24
 const TWO = 2
 
-const HOUR = MILLESECONDS_PER_SECOND * SECONDS_PER_MINUTE * MINUTES_PER_HOUR
+const SECOND = MILLESECONDS_PER_SECOND
+const MINUTE = MILLESECONDS_PER_SECOND * SECONDS_PER_MINUTE
+const HOUR = MINUTE * MINUTES_PER_HOUR
+const TWO_SECONDS = MILLESECONDS_PER_SECOND * TWO
 const TWO_DAYS = HOUR * HOURS_PER_DAY * TWO
 
 const MONTH_OFFSET = 1
@@ -19,9 +22,19 @@ const MONTH_OFFSET = 1
 const buildAgoString = (datetime: string) => {
   const updatedAt = new Date(datetime)
   const delta = new Date().getTime() - updatedAt.getTime()
+  console.log(delta)
 
+  if (delta < TWO_SECONDS) {
+    return 'now'
+  }
+  if (delta < MINUTE) {
+    return `${Math.floor(delta / SECOND)} seconds ago`
+  }
+  if (delta < HOUR) {
+    return `${Math.floor(delta / MINUTE)} minutes ago`
+  }
   if (delta < TWO_DAYS) {
-    return `${Math.ceil(delta / HOUR)} hours`
+    return `${Math.floor(delta / HOUR)} hours ago`
   }
   return `${updatedAt.getFullYear()}/${updatedAt.getMonth() + MONTH_OFFSET}/${updatedAt.getDate()}`
 }
