d43bb409ed2e9d75da0446f8ec9ccfbc2857b527
Takagi Kensuke <janus.wel.3@gmail.com>
2019-05-21 00:51:08 +0900

    :art: separate scenario into operations and taking evidences

--- modules/client/e2e/index.ts
+++ modules/client/e2e/index.ts
@@ -17,33 +17,44 @@ const SCREENSHOTS_DIR = 'screenshots'
 
 const buildSelector = (id: string) => `[data-testid="${id}"]`
 
+const operations = {
+  open: async (browser: Puppeteer.Browser) => {
+    const page = await openPage(browser, ENTRY_POINT, OPTIONS)
+    await page.waitForSelector(buildSelector('add-button'))
+    return page
+  },
+  add: async (page: Puppeteer.Page) => {
+    await page.type(buildSelector('input-title'), 'e2e')
+    await page.type(buildSelector('input-detail'), 'use puppeteer')
+    await page.click(buildSelector('add-button'))
+    await page.waitForSelector(buildSelector('todo-2'))
+  },
+  update: async (page: Puppeteer.Page) => {
+    await page.click(buildSelector('update-button-2'))
+    await clearInput(page, buildSelector('input-title-2'))
+    await page.type(buildSelector('input-title-2'), 'write an E2E test scenario')
+    await clearInput(page, buildSelector('input-detail-2'))
+    await page.type(buildSelector('input-detail-2'), 'use puppeteer\nrefer: https://pptr.dev/')
+    await page.click(buildSelector('modify-button-2'))
+    await page.waitForSelector(buildSelector('todo-2'))
+  },
+  delete: async (page: Puppeteer.Page) => {
+    await page.click(buildSelector('delete-button-1'))
+    await page.waitForSelector(buildSelector('network-indicator'), { hidden: true })
+  },
+}
+
 const runScenario = async (browser: Puppeteer.Browser) => {
-  const page = await openPage(browser, ENTRY_POINT, OPTIONS)
-  await page.waitForSelector(buildSelector('add-button'))
-  await page.screenshot({ path: `${SCREENSHOTS_DIR}/initial.png` })
+  const page = await operations.open(browser)
+  await page.screenshot({ path: `${SCREENSHOTS_DIR}/opened.png` })
 
-  await page.screenshot({ path: `${SCREENSHOTS_DIR}/initial.png` })
-
-  await page.type(buildSelector('input-title'), 'e2e')
-  await page.type(buildSelector('input-detail'), 'use puppeteer')
-  await page.click(buildSelector('add-button'))
-  await page.waitForSelector(buildSelector('todo-2'))
-
+  await operations.add(page)
   await page.screenshot({ path: `${SCREENSHOTS_DIR}/added.png` })
 
-  await page.click(buildSelector('update-button-2'))
-  await clearInput(page, buildSelector('input-title-2'))
-  await page.type(buildSelector('input-title-2'), 'write an E2E test scenario')
-  await clearInput(page, buildSelector('input-detail-2'))
-  await page.type(buildSelector('input-detail-2'), 'use puppeteer\nrefer: https://pptr.dev/')
-  await page.click(buildSelector('modify-button-2'))
-  await page.waitForSelector(buildSelector('todo-2'))
-
+  await operations.update(page)
   await page.screenshot({ path: `${SCREENSHOTS_DIR}/updated.png` })
 
-  await page.click(buildSelector('delete-button-1'))
-  await page.waitForSelector(buildSelector('network-indicator'), { hidden: true })
-
+  await operations.delete(page)
   await page.screenshot({ path: `${SCREENSHOTS_DIR}/delated.png` })
 }
 
